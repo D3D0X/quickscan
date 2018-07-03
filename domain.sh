789947f77c713ad543de66bbb70637f5db44b9cb
root <350t3r13@gmail.com>
2018-07-03 16:51:32 +0200

    Add

--- domain.sh
+++ domain.sh
@@ -4,7 +4,8 @@ extentions=`/bin/cat ext.txt`
 
 for i in $extentions
 do
-	chromium --no-sandbox $1/$i  
+#	chromium --no-sandbox $1/$i  
+	google-chrome --no-sandbox "http://"$1/$i  
 	sleep 1
 
 done
